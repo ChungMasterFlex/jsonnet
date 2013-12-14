a125102ec6f5ed44e873aed06eddff4e524a7621
chungmasterflex <strongworkandrewchung@gmail.com>
2013-12-14 09:46:12 -0800

    should be fine right now

---
+++ stable_2_with_2_args/MyAppTest/Test.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using AaronProject4;
+
+namespace MyAppTest
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			MainClass mC = new MainClass;
+			//Assert.IsNotEmpty(m.Main("chocolate" "San Francisco, CA");
+			Assert.IsNotEmpty(m.Main("chocolate" "San Francisco, CA");
+
+		}
+	}
+}
+
